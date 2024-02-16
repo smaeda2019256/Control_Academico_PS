fe2f94922f84aeb36b7c1161383a67610bca5160
smaeda-2019256 <smaeda-2019256@Kinal.edu.gt>
2024-02-16 08:35:19 -0600

    Se crea Get Maestros

--- api_smaeda_ControlAcademico/controllers/maestro.controller.js
+++ api_smaeda_ControlAcademico/controllers/maestro.controller.js
@@ -2,8 +2,24 @@ const bcryptjs = require('bcryptjs');
 const Maestro = require('../models/maestro');
 const { response, request } = require('express');
 
+const maestrosGet = async (req, res = response) =>{
+    const {limite, desde } = req.query;
+    const query = {estado: true};
+
+    const [total, maestros ] = await Promise.all([
+        Maestro.countDocuments(query),
+        Maestro.find(query)
+                .skip(Number(desde))
+                .limit(Number(limite))
+    ]);
+
+    res.status(200).json({
+        total,
+        maestros
+    });
+}
 
 
 module.exports = {
-
+    maestrosGet
 }
