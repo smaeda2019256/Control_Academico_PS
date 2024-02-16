ac6cbb5de869e77084ce3e89cd5ba87598ad5908
Santiago Maeda <smaeda-2019256@Kinal.edu.gt>
2024-02-15 18:58:42 -0600

    Se sube alumnos Get en controlador

--- api_smaeda_ControlAcademico/controllers/alumno.controller.js
+++ api_smaeda_ControlAcademico/controllers/alumno.controller.js
@@ -2,3 +2,23 @@ const bcryptsjs = require('bcryptjs');
 const Alumno = require('../models/alumno');
 const { response , request } = require('express');
 
+const alumnosGet = async (req, res = response) => {
+    const { limite, desde } = req.query;
+    const query = { estado: true };
+
+    const [total, alumnos] = await Promise.all([
+        Alumno.countDocuments(query),
+        Alumno.find(query)
+            .skip(Number(desde))
+            .limit(Number(limite))
+    ]);
+
+    res.status(200).json({
+        total,
+        alumnos
+    });
+}
+
+module.exports = {
+    alumnosGet
+}
