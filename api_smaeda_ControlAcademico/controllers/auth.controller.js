99b9131517acbad87c707672d7fbe819d46f8685
smaeda-2019256 <smaeda-2019256@Kinal.edu.gt>
2024-02-16 11:09:31 -0600

    Se crea verificador de login en auth controller

---
+++ api_smaeda_ControlAcademico/controllers/auth.controller.js
@@ -0,0 +1,54 @@
+const { request } = require('express');
+const Alumno = require('../models/alumno');
+const Maestro = require('../models/maestro');
+const bcryptjs = require('bcryptjs');
+const { generarJWT } = require('../helpers/generar-jwt');
+
+const login = async (req = request, res = response) => {
+    const { correo, password } = req.body;
+
+    try{
+        let user = await Alumno.findOne({ correo });
+
+        if (!user) {
+            user = await Maestro.findOne({ correo });
+
+            if (!user) {
+                return res.status(400).json({
+                    msg: "Credenciales incorrectas, el correo no existe en la DB!"
+                });
+            }
+        }
+
+        if (!user.estado) {
+            return res.status(400).json({
+                msg: "El usuario no existe en la DB"
+            });
+        }
+
+        const validarPassword = bcryptjs.compareSync(password, user.password);
+        if (!validarPassword) {
+            return res.status(400).json({
+                msg: "La contraseña es incorrecta."
+            });
+        }
+
+        const token = await generarJWT(user.id);
+
+        res.status(200).json({
+            msg: "Bienvenido",
+            user,
+            token
+        });
+
+    } catch(e) {
+        console.log(e);
+        res.status(500).json({
+            msg: "Comuniquese con el Administrador"
+        });
+    };
+};
+
+module.exports = {
+    login
+}
--- api_smaeda_ControlAcademico/models/server.js
+++ api_smaeda_ControlAcademico/models/server.js
@@ -1,21 +1,38 @@
 const express = require('express');
-//const cors = require('cors');
+const cors = require('cors');
 const { dbConnection } = require('../db/config');
 
 class Server {
-    constructor(){
+    constructor() {
         this.app = express();
         this.port = process.env.PORT;
+        this.alumnoPath = '/api/alumnos'
+        this.maestroPath = '/api/maestros'
+        this.loginPath = '/api/auth'
         this.conectarDB();
+        this.Middlewares();
+        this.routes();
     }
 
-    async conectarDB(){
+    async conectarDB() {
         await dbConnection();
     }
 
-    listen(){
-        this.app.listen(this.port, ()=> {
-            console.log("Servidor Conectado", this.port)
+    Middlewares() {
+        this.app.use(express.static('public'));
+        this.app.use(cors());
+        this.app.use(express.json());
+    }
+
+    routes() {
+        this.app.use(this.alumnoPath, require('../routes/alumno.routes'));
+        this.app.use(this.maestroPath, require('../routes/maestro.routes'));
+        this.app.use(this.loginPath, require('../routes/auth.routes'));
+    }
+
+    listen() {
+        this.app.listen(this.port, () => {
+            console.log('Servidor ejecutandose exitosamente en el puerto:', this.port)
         });
     }
 }
