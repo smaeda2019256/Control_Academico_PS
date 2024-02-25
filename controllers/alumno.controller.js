176c58f8127b65191a692690f33e751c28f0c079
smaeda2019256 <smaeda-2019256@Kinal.edu.gt>
2024-02-24 21:19:44 -0600

    Se realiza el PUT y DELETE del Alumno

--- controllers/alumno.controller.js
+++ controllers/alumno.controller.js
@@ -2,59 +2,35 @@ const bcrypt = require('bcrypt');
 const Alumno = require('../models/alumno');
 const { response , json } = require('express');
 
-const alumnosGet = async (req, res = response) => {
-    const { limite, desde } = req.query;
-    const query = { estado: true };
-
-    const [total, alumnos] = await Promise.all([
-        Alumno.countDocuments(query),
-        Alumno.find(query)
-            .skip(Number(desde))
-            .limit(Number(limite))
-    ]);
-
-    res.status(200).json({
-        total,
-        alumnos
-    });
-}
-
-const getAlumnoByid = async (req, res) => {
-    const { id } = req.params;
-    const alumno = await Alumno.findOne({_id: id});
-
-    res.status(200).json({
-        alumno
-    });
-}
-
 const putAlumnos = async (req, res = response) => {
     const { id } = req.params;
     const { _id, password, correo, ...resto } = req.body;
 
-    if (password) {
-        const salt = bcryptsjs.genSaltSync();
-        resto.password = bcryptsjs.hashSync(password, salt);
-    }
+    await Alumno.findByIdAndUpdate(id, resto)
 
-    const alumno = await Alumno.findByIdAndUpdate(id, resto);
+    const alumno = await Alumno.findOne({_id: id});
 
     res.status(200).json({
-        msg: 'El Alumno Fue Actualizado Exitosamente!',
+        msg: 'El Alumno Fue ACTUALIZADO Correctamente!',
         alumno,
     });
 }
 
 const alumnosDelete = async (req, res) => {
-    const {id} = req.params;
-    const alumno = await Alumno.findByIdAndUpdate(id, {estado: false});
-    const alumnoAutenticado = req.alumno;
+    try{
+        const  { id } = req.params;
 
-    res.status(200).json({
-        msg: 'Alumno que se Eliminara',
-        alumno,
-        alumnoAutenticado
-    });
+        const alumno = await Alumno.findByIdAndUpdate(id, {estado: false});
+        res.status(200).json({
+            msg: "El Alumno fue ELIMINADO Correctamente",
+            alumno
+        });
+
+    }catch (error) {
+        res.status(500).json({
+            msg: "El Alumno no se pudo ELIMINAR"
+        });
+    }
 }
 
 const alumnosPost = async (req, res) => {
