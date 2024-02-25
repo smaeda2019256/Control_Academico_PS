943966cb89051365eb0708caf4fcee342dc909db
smaeda2019256 <smaeda-2019256@Kinal.edu.gt>
2024-02-25 10:22:59 -0600

    Se verifica en el jwt que el Maestro exista

--- middlewares/validar-jwt.js
+++ middlewares/validar-jwt.js
@@ -3,7 +3,7 @@ const Maestro = require('../models/maestro');
 const jwt = require('jsonwebtoken');
 const { request, response } = require('express');
 
-const validarJWT = async(req = request, res = response, next) => {
+const validarJWT = async (req = request, res = response, next) => {
     const token = req.header('x-token');
 
     if (!token) {
@@ -11,31 +11,53 @@ const validarJWT = async(req = request, res = response, next) => {
             msg: 'No hay TOKEN en la petición',
         });
     }
-    try{
+    try {
         const { uid } = jwt.verify(token, process.env.SECRETORPRIVATEKEY);
+        const maestro = await Maestro.findById(uid);
         const alumno = await Alumno.findById(uid);
-        if(!alumno){
-            return res.status(401).json({
-                msg: 'El Alumno no existe en la Base de Datos'
+
+        if (maestro) {
+            if (!maestro) {
+                return res.status(400).json({
+                    msg: 'El Maestro no EXISTE'
+                });
+            }
+            if (!maestro.estado) {
+                return res.status(401).json({
+                    msg: 'Token Válido - Maestro con Estado FALSE'
+                });
+            }
+            if (maestro) {
+                req.maestro = maestro;
+                req.correo = maestro.correo;
+            }
+
+        } else {
+            if (!alumno) {
+                return res.status(400).json({
+                    msg: 'El Alumno no existe en la Base de Datos'
+                });
+            }
+
+            if (!alumno.estado) {
+                return res.status(401).json({
+                    msg: 'TOKEN no válido, Alumno con estado FALSE'
+                });
+            };
+
+            if (alumno) {
+                req.alumno = alumno;
+            }
+        }
+            next();
+
+        }catch (e) {
+            console.log(e);
+            res.status(401).json({
+                smg: 'TOKEN no válido'
             });
         }
-
-        if(!alumno.estado){
-            return res.status(401).json({
-                msg: 'TOKEN no válido, Alumno con estado false'
-            });
-        };
-
-        req.alumno = alumno;
-        next();
-
-    }catch(e){
-        console.log(e);
-        res.status(401).json({
-            smg: 'TOKEN no válido'
-        });
     }
-}
 
 module.exports = {
     validarJWT
